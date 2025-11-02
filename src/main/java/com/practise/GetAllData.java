afa43b090f66f0bf0121df61fee91715338e79a7
HP <riyasawwalakhe@gmail.com>
2025-11-02 12:51:13 +0530

    message commit

--- src/main/java/com/practise/GetAllData.java
+++ src/main/java/com/practise/GetAllData.java
@@ -18,7 +18,7 @@ public class GetAllData {
 
 		List<Vehicles> vehicleList = session.createQuery("select v FROM Vehicles v", Vehicles.class).getResultList();
 
-		System.out.println("List of vehicles: ");
+		System.out.println("List of vehicles are: ");
 		vehicleList.stream().forEach(k -> System.out.println(k.toString()));
 
 		transaction.commit();
