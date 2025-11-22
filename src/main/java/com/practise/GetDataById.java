6d15ca14d0a8fc7e325cce6ad011f1c5bd36b432
HP <riyasawwalakhe@gmail.com>
2025-11-22 13:56:00 +0530

    message commit

--- src/main/java/com/practise/GetDataById.java
+++ src/main/java/com/practise/GetDataById.java
@@ -14,7 +14,7 @@ public static void main(String[] args) {
 		SessionFactory factory = HibernateUtility.getSessionFactory();
 		Session session = factory.openSession();
 		Transaction transaction = session.beginTransaction();
-		
+		System.out.println("Vehicle detail are: ");
 		int id = 1;
 		Vehicles vehicles = session.find(Vehicles.class, 1);
 		
